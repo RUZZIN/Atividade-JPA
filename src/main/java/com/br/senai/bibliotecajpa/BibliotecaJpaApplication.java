3076f4658861409ef1e02e9e94afab3c43c04e4e
Ruan Pablo <ruanp9725@gmail.com>
2025-03-19 21:28:10 -0300

    Envio do Swagger configurado

--- src/main/java/com/br/senai/bibliotecajpa/BibliotecaJpaApplication.java
+++ src/main/java/com/br/senai/bibliotecajpa/BibliotecaJpaApplication.java
@@ -1,20 +1,27 @@
 package com.br.senai.bibliotecajpa;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
-		info = @io.swagger.v3.oas.annotations.info.Info(title = "${info.build.name}",
-				version = "${info.build.version}",
-				description = "${info.app.description}",
-		contact = @io.swagger.v3.oas.annotations.info.Contact(name = "Time Arquitetura e APIs", email = "jonatas.edo@gmail.com")))
-
+    info = @Info(
+        title = "${info.build.name}",
+        version = "${info.build.version}",
+        description = "${info.app.description}",
+        contact = @Contact(
+            name = "Time Arquitetura e APIs",
+            email = "ruanp9725@gmail.com"
+        )
+    )
+)
 @SpringBootApplication
 public class BibliotecaJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaJpaApplication.class, args);
+    }
 
 }
